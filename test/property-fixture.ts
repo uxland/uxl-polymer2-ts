74a831a1362c659efd6d7cd9343ea7d40e6479a6
Alex Vizcaino <avizcaino@uxland.es>
2018-08-02 08:15:24 +0200

    polymer test

--- test/property-fixture.ts
+++ test/property-fixture.ts
@@ -1,41 +1,38 @@
-import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
-import {computed, customElement, property} from "../src/index";
+import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
+import { computed, customElement, property } from "../src/index";
 const should = chai.should();
 //import {suite} from "mocha";
-suite('property fixture', () =>{
-
-    test('property created', () =>{
-        @customElement('my-element')
-        class Component extends PolymerElement{
-            @property()
-            myProperty: string;
+suite("property fixture", () => {
+    test("property created", () => {
+        @customElement("my-element")
+        class Component extends PolymerElement {
+            @property() myProperty: string;
         }
-        should.exist(Component['properties']['myProperty']);
-        should.equal(Component['properties']['myProperty'].type, String);
+        should.exist(Component["properties"]["myProperty"]);
+        should.equal(Component["properties"]["myProperty"].type, String);
     });
-    test('computed default value', () =>{
-        @customElement('my-element1')
-        class Component extends PolymerElement{
+    test("computed default value", () => {
+        @customElement("my-element1")
+        class Component extends PolymerElement {
             static get template() {
-                return html `<h1 id="header">hello</h1>`;
+                return html`<h1 id="header">hello</h1>`;
             }
             myDefValue: boolean;
-            @property()
-            myProperty: string;
-            @computed('myDefValue', ['myProperty'], undefined, Boolean, false)
-            computeMyProperty(p){
-                return p == 'hello';
+            @property() myProperty: string;
+            @computed("myDefValue", ["myProperty"], undefined, Boolean, false)
+            computeMyProperty(p) {
+                return p == "hello";
             }
-            connectedCallback(){
+            connectedCallback() {
                 super.connectedCallback();
             }
         }
-        const fixtureElementName = 'polymer-decorators-fixture';
+        const fixtureElementName = "polymer-decorators-fixture";
         let container: HTMLDivElement = fixture(fixtureElementName);
-        let component: Component = <any>document.createElement('my-element1');
+        let component: Component = <any>document.createElement("my-element1");
         container.appendChild(<any>component);
         should.equal(component.myDefValue, false);
-        component.myProperty = 'hello';
+        component.myProperty = "hello";
         should.equal(component.myDefValue, true);
     });
 });
