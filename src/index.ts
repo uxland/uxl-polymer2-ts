a0844a9a7dfb39cd7fa23b5afd91185305d9c1ce
dpinart <dpinart@newuibcn.com>
2018-09-19 16:20:00 +0200

    fix: listen decorator refactor

--- src/index.ts
+++ src/index.ts
@@ -99,28 +99,31 @@ function addReadyHandler(proto: any) {
         handlers.forEach(handler => handler.node.removeEventListener(handler.eventName, handler.handler));
         this.__listenersEventHandlers = [];
     };
-    proto.updated = function(changedProperties){
-        if(updated)
-            updated.call(this, changedProperties);
+    proto.__subscribeToListeners = function(){
         this.__unsubscribeFromListeners();
-        let handlers: EventHandler[] = [];
-        (proto.__listeners || []).forEach(v =>{
-            let nodes:EventTarget[] = [] ;
-            if(typeof v.target === 'string'){
-                let queryResult: NodeListOf<Node> = this.shadowRoot.querySelectorAll(v.target);
+        let handlers = [];
+        (proto.__listeners || []).forEach(v => {
+            let nodes = [];
+            if (typeof v.target === 'string') {
+                let queryResult = this.shadowRoot.querySelectorAll(v.target);
                 queryResult.forEach(n => nodes.push(n));
                 queryResult = this.querySelectorAll(v.target);
                 queryResult.forEach(n => nodes.push(n));
             }
-
-            else nodes.push(v.target);
+            else
+                nodes.push(v.target);
             nodes.forEach(node => {
-                let handler: EventHandler = {eventName: v.eventName, node, handler: ((e) => this[v.functionKey](e)).bind(this)};
+                let handler = { eventName: v.eventName, node, handler: ((e) => this[v.functionKey](e)).bind(this) };
                 node.addEventListener(v.eventName, handler.handler);
-                handlers.push(handler)
+                handlers.push(handler);
             });
         });
         this.__listenersEventHandlers = handlers;
+    };
+    proto.updated = function(changedProperties){
+        if(updated)
+            updated.call(this, changedProperties);
+        this.__subscribeToListeners();
     };
     proto.disconnectCallback = function(){
         if(disconnectCallback)
